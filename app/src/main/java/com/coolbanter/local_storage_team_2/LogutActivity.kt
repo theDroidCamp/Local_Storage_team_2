3ffeed448eed091fda605240c1f0955c7b3000a6
carluvy <37089288+carluvy@users.noreply.github.com>
2020-11-03 21:31:42 +0300

    Added required activities

---
+++ app/src/main/java/com/coolbanter/local_storage_team_2/LogutActivity.kt
@@ -0,0 +1,11 @@
+package com.coolbanter.local_storage_team_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logut)
+    }
+}
