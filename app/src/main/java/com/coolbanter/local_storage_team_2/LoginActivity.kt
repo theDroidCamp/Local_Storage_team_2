149b81fe87c4fc852921a4db9faa632d4da276c8
carluvy <37089288+carluvy@users.noreply.github.com>
2020-11-05 22:07:07 +0300

    ViewBinding added

--- app/src/main/java/com/coolbanter/local_storage_team_2/LoginActivity.kt
+++ app/src/main/java/com/coolbanter/local_storage_team_2/LoginActivity.kt
@@ -1,11 +1,30 @@
 package com.coolbanter.local_storage_team_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.coolbanter.local_storage_team_2.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, LogoutActivity::class.java))
+            finish()
+        
+
+    }
+    
+
+
+
+
     }
 }
--- app/src/main/java/com/coolbanter/local_storage_team_2/SignUpActivity.kt
+++ app/src/main/java/com/coolbanter/local_storage_team_2/SignUpActivity.kt
@@ -1,11 +1,28 @@
 package com.coolbanter.local_storage_team_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.coolbanter.local_storage_team_2.databinding.ActivitySignUpBinding
+
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.btnSignUp.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
     }
+
+
+
+
 }
