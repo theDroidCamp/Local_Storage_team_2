31d9500977db68940639ace493fa77e7a028ed1f
carluvy <37089288+carluvy@users.noreply.github.com>
2020-11-08 01:10:00 +0300

    Logic for the logout screen. Added conditions for sign up and login

--- app/src/main/java/com/coolbanter/local_storage_team_2/LoginActivity.kt
+++ app/src/main/java/com/coolbanter/local_storage_team_2/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.coolbanter.local_storage_team_2.databinding.ActivityLoginBinding
+
 
 
 class LoginActivity : AppCompatActivity() {
@@ -14,42 +16,61 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
             
         
 
       binding.btnLogin.setOnClickListener {
-            saveData()
+          saveData()
 
-            //This will change the current screen to another screen
-            startActivity(Intent(this, LogoutActivity::class.java))
-            finish()
+          //This will change the current screen to another screen
+
+
+
+
+
         }
     }
         //To save data with shared preference
     private fun saveData() {
             //If field is empty will show (Input email address)
-        if (binding.emailAddressEt.text!!.isEmpty()) {
-            binding.emailAddressEt.error = "Input your email address!!"
-            return
-        }
-            //If field is empty will show (Password is empty)
-        if (binding.passwordEt.text!!.isEmpty()) {
-            binding.passwordEt.error = "Password is empty!"
-            return
-        }
+                if (binding.emailAddressEt.text!!.isEmpty()) {
+                    binding.emailAddressEt.error = "Input your email address!!"
+                    return
+                }
+                //If field is empty will show (Password is empty)
+                if (binding.passwordEt.text!!.isEmpty()) {
+                    binding.passwordEt.error = "Password is empty!"
+                    return
+                }
 
-        val mypref = getSharedPreferences("mypref", Context.MODE_PRIVATE)
 
-        val editor = mypref.edit()
+            val mypref = getSharedPreferences("mypref", Context.MODE_PRIVATE)
 
-        editor.putString("name", binding.emailAddressEt.text.toString())
-        editor.putString("password", binding.emailAddressEt.text.toString())
+            val email = mypref.getString("email", "")
+            val password = mypref.getString("password", "")
+
+            if (email!! == binding.emailAddressEt.text.toString() && password!! == binding.passwordEt.text.toString()) {
+                    startActivity(Intent(this, LogoutActivity::class.java))
+                    finish()
+            } else {
+                Toast.makeText(this, "Wrong details!", Toast.LENGTH_LONG).show()
+            }
+
+//            val editor = mypref.edit()
+//
+//
+//            editor.putString("email", binding.emailAddressEt.text.toString())
+//            editor.putString("password", binding.emailAddressEt.text.toString())
+//            editor.apply()
+
 
     }
             //To retrieve data with shared preference
-    private fun retrievedata() {
-        val mypref = getSharedPreferences("mypref", Context.MODE_PRIVATE)
-    }
+//    private fun retrievedata() {
+//        val mypref = getSharedPreferences("mypref", Context.MODE_PRIVATE)
+//    }
     
 
 
