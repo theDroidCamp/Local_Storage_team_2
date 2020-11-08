7f0c699d6731164bc92e0245ffe2a526eb681523
taofeeq23 <50840385+taofeeq23@users.noreply.github.com>
2020-11-08 13:06:11 +0100

    Added Login Button to the Sign Up Screen

--- app/src/main/java/com/coolbanter/local_storage_team_2/LoginActivity.kt
+++ app/src/main/java/com/coolbanter/local_storage_team_2/LoginActivity.kt
@@ -1,22 +1,28 @@
-package com.coolbanter.local_storage_team_2
+ package com.coolbanter.local_storage_team_2
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.coolbanter.local_storage_team_2.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
+
+//    lateinit var testFieldEditTest: EditText
+//    lateinit var passwordEdit: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
             
-        
+//        testFieldEditTest = binding.emailAddressEt
+//        passwordEdit = binding.passwordEt
 
-      binding.loginButton.setOnClickListener {
+      binding.btnLogin.setOnClickListener {
             savedata()
 
             //This will change the current screen to another screen
@@ -27,13 +33,13 @@ class LoginActivity : AppCompatActivity() {
         //To save data with shared preference
     private fun savedata() {
             //If field is empty will show (Input email address)
-        if (binding.testFieldEditTest.text!!.isEmpty()) {
-            binding.testFieldEditTest.error = "Input your email address!!"
+        if (binding.emailAddressEt.text!!.isEmpty()) {
+            binding.emailAddressEt.error = "Input your email address!!"
             return
         }
             //If field is empty will show (Password is empty)
-        if (binding.passwordEdit.text!!.isEmpty()) {
-            binding.passwordEdit.error = "Password is empty!"
+        if (binding.passwordEt.text!!.isEmpty()) {
+            binding.passwordEt.error = "Password is empty!"
             return
         }
 
@@ -41,17 +47,13 @@ class LoginActivity : AppCompatActivity() {
 
         val editor = mypref.edit()
 
-        editor.putString("name", binding.testFieldEditTest.text.toString())
-        editor.putString("password", binding.passwordEdit.text.toString())
+        editor.putString("name", binding.emailAddressEt.text.toString())
+        editor.putString("password", binding.passwordEt.text.toString())
 
     }
             //To retrieve data with shared preference
     private fun retrievedata() {
         val mypref = getSharedPreferences("mypref", Context.MODE_PRIVATE)
     }
-    
 
-
-
-    }
 }
